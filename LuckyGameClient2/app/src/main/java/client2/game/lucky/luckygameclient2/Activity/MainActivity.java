96a671fa4f973c7f98d10b8354b576d78c190c4e
michalfujak <michalfujak005@gmail.com>
2019-12-28 18:48:46 +0100

    Update design fonts2 and background purple

---
+++ LuckyGameClient2/app/src/main/java/client2/game/lucky/luckygameclient2/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package client2.game.lucky.luckygameclient2.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import client2.game.lucky.luckygameclient2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ LuckyGameClient2/app/src/main/java/client2/game/lucky/luckygameclient2/Common/Common.java
@@ -0,0 +1,8 @@
+package client2.game.lucky.luckygameclient2.Common;
+
+public class Common {
+
+    // static variable
+    public static int score = 1000; // Virtual value, is fake score.
+
+}
